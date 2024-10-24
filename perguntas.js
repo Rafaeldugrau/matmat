165e06f11eb477bf2e7d8892ca8abe7f4f0ab9e1
Rafaeldugrau <00001089045803sp@al.educacao.sp.gov.br>
2024-10-24 12:03:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a é o maior estado do ESTADOS UNIDOS?',
+    'o maior estado é o alasca'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'quais são as raizes da segunte equação x²+6x+5=0?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
